2e8d300adae7e502a2379f84b10cb73a6ff97d51
henryhoangduong <henryhoangduong@gmail.com>
2025-02-01 13:18:39 +0700

    generate token

--- src/main/java/com/example/Todo/security/JwtTokenProvider.java
+++ src/main/java/com/example/Todo/security/JwtTokenProvider.java
@@ -1,8 +1,35 @@
 package com.example.Todo.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import org.slf4j.Logger;
+
+import java.util.Date;
+
 
 @Component
 public class JwtTokenProvider {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtTokenProvider.class);
+    @Value(value = "${app.jwtSecret}")
+    private String jwtSecret;
 
+    @Value(value = "${app.jwtExpirationInMs}")
+    private int jwtExpirationInMs;
+
+    public String generateToken(Authentication authentication) {
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+return Jwts.builder()
+        .setSubject(Long.toString(userPrincipal.getId()))
+        .setIssuedAt(new Date())
+        .setExpiration(expiryDate)
+        .signWith(SignatureAlgorithm.HS512, jwtSecret)
+        .compact();
+    }
 }
---
+++ src/main/java/com/example/Todo/security/UserPrincipal.java
@@ -0,0 +1,63 @@
+package com.example.Todo.security;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class UserPrincipal implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    @JsonIgnore
+    private String email;
+
+    @JsonIgnore
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserPrincipal(Long id, String firstName, String lastName, String username, String email, String password,
+                         Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+
+        if (authorities == null) {
+            this.authorities = null;
+        } else {
+            this.authorities = new ArrayList<>(authorities);
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities == null ? null : new ArrayList<>(authorities);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+}
