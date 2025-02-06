7078498a4bc2d23e9d03010d2995274e7d2d5e8b
henryhoangduong <henryhoangduong@gmail.com>
2025-02-06 14:14:35 +0700

    Category entity

--- src/main/java/com/example/Todo/model/Category.java
+++ src/main/java/com/example/Todo/model/Category.java
@@ -1,12 +1,47 @@
 package com.example.Todo.model;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @EqualsAndHashCode
 @Entity
 @Data
-@Entity
+@Table(name = "categories")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Category {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts;
+
+    public Category(String name) {
+        super();
+        this.name = name;
+    }
+
+    public List<Post> getPosts() {
+        return this.posts == null ? null : new ArrayList<>(this.posts);
+    }
+
+    public void setPosts(List<Post> posts) {
+        if (posts == null) {
+            this.posts = null;
+        } else {
+            this.posts = Collections.unmodifiableList(posts);
+        }
+    }
 }
