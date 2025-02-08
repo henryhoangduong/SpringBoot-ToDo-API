d482af858cee99062c1970c978e44a5407b5e2e7
henryhoangduong <henryhoangduong@gmail.com>
2025-02-08 23:18:53 +0700

    finish repository

---
+++ src/main/java/com/example/Todo/repository/TagRepository.java
@@ -0,0 +1,9 @@
+package com.example.Todo.repository;
+
+import com.example.Todo.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
